2994844ffbeff54be16313bb07479ed3642ff33d
Alehegne Geta <alehegngetaenyewuu@gmail.com>
2026-03-27 19:18:22 +0300

    Add backtracking solution for combinations problem
    Implement backtracking algorithm to find combinations of k numbers from 1 to n.

---
+++ leetcode/backtrack/77. Combinations.py
@@ -0,0 +1,19 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+
+
+        ans = []
+
+        def backtrack(start,path):
+            nonlocal ans
+            if len(path) == k:
+                ans.append(path[:])
+                return
+            
+            for i in range(start,n+1):
+                path.append(i)
+                backtrack(i+1,path)
+                path.pop()
+        backtrack(1,[])
+        return ans
+        
