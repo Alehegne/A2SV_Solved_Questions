51758b63df64d75f3b6dd44a50d4db03de69b18e
Alehegne Geta <alehegngetaenyewuu@gmail.com>
2026-04-15 17:00:17 +0300

    Add solution for Search a 2D Matrix

--- leetcode/0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ leetcode/0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -2,25 +2,31 @@ class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
 
 
-        flatten = [el for row in matrix for el in row]
-        
-        def bins(l,r):
-            nonlocal flatten
-
-            mid = (l + r)//2
-            if flatten[mid] == target:
-                return True
-            
-            if l > r:
-                return False
-            
-            if flatten[mid] > target:
-                return bins(l,mid-1)
-            else:
-                return bins(mid+1,r)
-
-
-        return bins(0,len(flatten)-1)
+        def get_row():
+            l,r = 0,len(matrix)-1
+            ans = 0
+            while l <= r:
+                mid = (l + r)//2
+                if target < matrix[mid][0]:
+                    r = mid - 1
+                else:
+                    ans = mid
+                    l = mid + 1
+            return ans
+        def get_ele(row):
+            l,r = 0,len(matrix[row])-1
+            while l <= r:
+                mid = (l+r)//2
+                if target == matrix[row][mid]:
+                    return True
+                elif target > matrix[row][mid]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+            return False
+        row = get_row()
+        print("row:",row)
+        return get_ele(row)
 
 
         
